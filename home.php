d359dccfe628f5c6733aa5dee31d784bd0ea1a0d
starkwebs <hemchandsharma948@gmail.com>
2020-08-12 17:24:21 +0530

    my first website

---
+++ home.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<style type="text/css">
+	body{
+		margin:0; padding:0; box-sizing: border-box;
+	}
+	.topnav{
+		overflow: hidden;
+		background-color: #333;
+	}
+	.topnav a{
+		float:left;
+		color:#f2f2f2;
+		text-align: center;
+		padding: 14px 16px;
+		text-decoration: none;
+		font-size: 17px;
+	}
+	.topnav a:hover{
+		background-color: #ddd;
+		color:black;
+	}
+	.topnav a.active{
+		background-color: #4CAF50;
+		color:white;
+	}
+</style>
+<body>
+
+<div class="topnav">
+	<a class="active" href="#home">Home</a>
+	<a class="active" href="">News</a>
+	<a href="#contact">Contact</a>
+	<a href="#about">About</a>
+</div>
+
+</body>
+</html>
